51496de7f159d5b92d2ad84db6996e2ce9dda71e
tony stark <mehul.chopra.dev@gmail.com>
2020-03-11 22:30:40 +0530

    day 5

--- com/xyz/lib/student.py
+++ com/xyz/lib/student.py
@@ -1,2 +1,31 @@
+# Developer X
 class Student:
-    pass
+
+    def __init__(self, name, roll, marks, gender):
+        # Constructor
+
+        # object attributes
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+        self.gender = gender
+
+    def get_details(self):
+        # self - current object for which the "outside user" called this method
+        return 'Name : ' + self.name + '\nGender: ' + self.gender\
+            + '\nRoll: ' + str(self.roll) + '\nMarks: ' + str(self.marks)
+
+    def get_grade(self):
+        marks = self.marks
+        if marks > 100 or marks < 0:
+            grade = 'I'
+        elif marks >= 70:
+            grade = 'A'
+        elif marks >= 60:
+            grade = 'B'
+        elif marks >= 40:
+            grade = 'C'
+        else:
+            grade = 'F'
+
+        return grade
--- student_oop.py
+++ student_oop.py
@@ -1,17 +1,32 @@
+# Developer Y
+
 from com.xyz.lib.student import Student
 
-s1 = Student() # 3004 (RAM)
-s1.name = 'mehul chopra'
+# s1 = Student() # 3004 (RAM)
+'''
+Internally
+1. Reserve memory in the RAM for the Student object (3004)
+2. Student.__init__(3004)
+'''
+
+''' s1.name = 'mehul chopra'
 s1.roll = 10
 s1.marks = 90.45
-s1.gender = 'm'
+s1.gender = 'm' '''
+
+s1 = Student('mehul chopra', 10, 90.45, 'm')
+'''
+Internally
+1. Reserve memory in the RAM for the Student object (3004)
+2. Student.__init__(3004, 'mehul chopra', 10, 90.45, 'm')
+'''
 
 
-s2 = Student() # 3006 (RAM)
-s2.name = 'jane'
+s2 = Student('jane', 12, 45, 'f') # 3006 (RAM)
+''' s2.name = 'jane'
 s2.roll = 12
-s2.marks = 78
-s2.gender = 'f'
+s2.marks = 45
+s2.gender = 'f' '''
 
 # print(s1)
 # print(s2)
@@ -23,4 +38,25 @@ print(s2.name)
 print(s2.roll) '''
 
 print(s1.get_details())
+# Python Internally
+# print(Student.get_details(s1))
+
 print(s2.get_details())
+# Internally
+# print(Student.get_details(s2))
+
+print(s1.get_grade())
+# Internally
+# print(Student.get_grade(s1))
+
+print(s2.get_grade())
+# Internally
+# print(Student.get_grade(s2))
+
+s3 = Student('jill', 15, 34, 'f') # 3009 (RAM)
+''' s3.s_name = 'jill'
+s3.r = 15
+s3.marks = 34
+s3.gen = 'f' '''
+
+print(s3.get_details())
